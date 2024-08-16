0c4419abf7baf408a26e2b32076af533ff3a5199
HongYeonSeung <yshong2332@gmail.com>
2024-08-17 02:19:25 +0900

    테일윈드CSS 설치

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // 리액트 프로젝트의 소스 코드 경로
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
